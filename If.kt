66a404396c8118ebbb7d5a41e0083b620bcf914c
GarenLiang <ideabreeze@gmail.com>
2017-05-30 21:17:58 -0700

    If and Else

---
+++ If.kt
@@ -0,0 +1,27 @@
+/**
+ * Created by garenliang on 2017/5/26.
+ */
+
+
+fun main(args : Array<String>) {
+//    var num1 : Int = 4
+//    var num2 : Int = 7
+//
+//    var result : Int = 0
+//
+//    result = if(num1 > num2)
+//        num1
+//    else if(num1 < num2)
+//        num2
+//    else
+//        0
+//
+//    println(result)
+    var str1 : String = "Navin"
+    var str2 : String = "navin"
+
+    if(str1 == str2)
+        println("same")
+    else
+        println("not same")
+}
