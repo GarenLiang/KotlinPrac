8f4af9d001a3c9a8de607407a04c6a5684e0acc0
GarenLiang <ideabreeze@gmail.com>
2017-05-30 23:03:01 -0700

    Map

---
+++ Map.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main(args : Array<String>) {
+//    var nums = listOf(1,2,3,4)
+//
+//    for((i,e) in nums.withIndex()) {
+//        println(" $i : $e")
+//    }
+    var aliens = TreeMap<String,Int>()
+    aliens["Navin"] = 543
+    aliens["Kishor"] = 409
+
+    for((name,age) in aliens) {
+        println("$name : $age")
+    }
+}
